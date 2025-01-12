3508d9bc5ad19370db6023b5d07853b2290e986a
Halley <Halley.z.Comet@outlook.com>
2025-01-12 16:25:14 +0000

    parallax effect

--- homepage.js
+++ homepage.js
@@ -1,22 +1,12 @@
-const parallax_el = document.querySelectorAll('.bg, .stop, .double, .triangle');
+const parallax_el = document.querySelectorAll('.parallax');
 
 window.addEventListener("mousemove", (o) => {
     const xValue = (o.clientX - window.innerWidth / 2) / 2;
     const yValue = (o.clientY - window.innerHeight / 2) / 2;
 
-    // parallax_el.forEach((el) => {
-    //     el.style.transform = `translateX(calc(-50% + ${-xValue}px)) translateY(calc(-50% + ${yValue}px))`;
-    // });
-    updateTransform(o, 0, ".bg", 70);
-    updateTransform(o, 15, ".stop", 20);
-    updateTransform(o, 25, ".double", 15);
-    updateTransform(o, -35, ".triangle", 8);
-});
-
-function updateTransform(mouse, rotateValue, className, factor) {
-    const xValue = (mouse.clientX - window.innerWidth / 2) / factor;
-    const yValue = (mouse.clientY - window.innerHeight / 2) / factor;
-    const image = document.querySelector(className);
-
-    image.style.transform = `  translateX(calc( ${-xValue}px)) translateY(calc(${yValue}px)) rotate(${rotateValue}deg) translate(-50%, -50%)     `;
-}
+    parallax_el.forEach(el => {
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+    })
+})
