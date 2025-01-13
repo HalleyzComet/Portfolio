1922ea2ee87e901271b2d69d67c14d53f373bc28
Halley <Halley.z.Comet@outlook.com>
2025-01-13 18:58:46 +0000

    stuff

--- homepage.js
+++ homepage.js
@@ -1,12 +1,37 @@
 const parallax_el = document.querySelectorAll('.parallax');
 
-window.addEventListener("mousemove", (o) => {
-    const xValue = (o.clientX - window.innerWidth / 2) / 2;
-    const yValue = (o.clientY - window.innerHeight / 2) / 2;
+let xValue = 0, yValue = 0;
 
-    parallax_el.forEach(el => {
+let rotateDegree = 0;
+
+function update(CursorPosition) {
+    parallax_el.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
-        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
-    })
-})
+        let speedz = el.dataset.speedz;
+        let rotateSpeed = el.dataset.rotation;
+
+        let isInLeft =
+         parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+
+         let zValue = (CursorPosition - parseFloat(getComputedStyle(el).left)) *
+         isInLeft * 0.1;
+
+        el.style.transform = 
+        `translateX(calc(-50% + ${-xValue * speedx}px)) 
+        translateY(calc(-50% + ${yValue * speedy}px)) 
+        perspective(2300px) translateZ(${-zValue * speedz}px)
+        rotateY(${rotateDegree * rotateSpeed}deg)`;
+    });
+}
+
+update(0);
+
+window.addEventListener("mousemove", (o) => {
+  xValue = o.clientX - window.innerWidth / 2;
+  yValue = o.clientY - window.innerHeight / 2;
+
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+    update(o.clientX);
+});
